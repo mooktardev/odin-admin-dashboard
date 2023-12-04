b3a743ae64083b3d58e45227e33680ff39e81ec2
MooktarDev <mooktardev1@gmail.com>
2023-12-04 21:03:12 +0100

    Ajout du html et stylisation avec tailwindcss

---
+++ README.md
@@ -0,0 +1,22 @@
+# Admin Dashboard
+This is an assignement from The Odin Projects.
+
+
+## Screenshot
+![Screenshot](./screenshot.png)
+
+
+## Demo
+[Live preview](https://admin-dashboard-odin.vercel.app)
+
+
+## Tech Stack
+HTML5 and CSS3 (*TailwindCSS*)
+
+
+## Author
+- [@mooktardev](https://www.github.com/mooktardev)
+
+
+## Licence
+MIT
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        }
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      }
+    },
   },
   plugins: [],
 }
